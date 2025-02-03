b84acb53e3ccacc0ce4edf898c6a54e7cdeecdc6
elyseeelius <elyseeelius@outlook.com>
2025-02-02 19:47:41 -0500

    added the inventory csv to the database

--- app.py
+++ app.py
@@ -1,4 +1,59 @@
-from models import (Base, session, Product, engine)
+from models import (Base, session, 
+                    Product, engine)
+import csv
+import datetime
+
+
+def menu():
+    while True:
+        print('''\nPROGRAMMING BOOKS
+              \r1) Add books
+              \r2) View all books
+              \r3) Search for book
+              \r4) Book Analysis
+              \r5) Exit ''')
+        choice = input('What would you like to do? ')
+        if choice in ['1', '2','3','4','5']:
+            return choice
+        else:
+            input('''
+                  Please choose one of the options above.
+                  A number from 1 to 5
+                  Please try again. ''')
+
+
+def clean_date(date_str):
+    split_date = date_str.split('/')
+    year = int(split_date[2])
+    month = int(split_date[0])
+    day = int(split_date[1])
+    return datetime.date(year, month, day)
+   
+def clean_price(price_str):
+    slice_price = price_str[1:]
+    float_price = float(slice_price)
+    return int(float_price * 100)
+    
+    
+    
+    
+
+def add_csv():
+    with open('inventory.csv') as csvfile:
+        data = csv.reader(csvfile)
+        for row in data:
+            product_in_db = session.query(Product).filter(Product.product_name==row[0]).one_or_none()
+            if product_in_db == None:
+                product_name = row[0]
+                product_price = clean_price(row[1])
+                product_quantity = row[2]
+                date_updated = clean_date(row[3])
+                new_product = Product(product_name=product_name, product_price=product_price, product_quantity=product_quantity, date_updated= date_updated)
+                session.add(new_product)
+        session.commit()
+
+        
+            
 
 
 
@@ -10,12 +65,27 @@ from models import (Base, session, Product, engine)
 
 
 
-
-
-
+def app():
+    app_running = True
+    while app_running:
+        choice = menu()
+        if   choice == '1':
+            pass
+        elif choice == '2':
+            pass
+        elif choice == '3':
+            pass
+        elif choice == '4':
+            pass
+        else:
+            print('GOODBYE!')
+            app_running = False
 
 
 
 
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
+    add_csv()
+    for product in session.query(Product):
+        print(product)
--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 from sqlalchemy import (create_engine, Column, 
                         Integer, String, Date)
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 
 engine = create_engine('sqlite:///inventory.db', echo=False)
@@ -10,12 +9,12 @@ session = Session()
 Base = declarative_base()
 
 class Product(Base):
-    __tablename__ = 'books'
-    product_id = Column(Integer, primary_key=True)
-    product_name = Column('Product Name',String)
-    product_quantity = Column('Product Quantity',String)
-    product_price = Column('Product Price', Date)
-    date_updated = Column('Date Updated', Integer)
+    __tablename__ = 'products'
+    id = Column(Integer, primary_key=True)
+    product_name = Column(String)
+    product_price = Column(Integer)
+    product_quantity = Column(Integer)
+    date_updated = Column(Date)
 
     def __repr__(self):
-        return f'Product Name: {self.product_name}, Product Quantity: {self.product_quantity}, Product Price: {self.product_price}, Date Updated: {self.date_updated}'
+        return f'{self.product_name} | {self.product_quantity} | {self.product_price} | {self.date_updated}'
