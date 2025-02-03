88dc805ac56a313b1b09472312b2f18213cc944a
elyseeelius <elyseeelius@outlook.com>
2025-02-03 06:55:54 -0500

    added product to the inventory functinality

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from models import (Base, session,
                     Product, engine)
 import csv
 import datetime
+import time
 
 
 def menu():
@@ -24,15 +25,35 @@ def menu():
 
 def clean_date(date_str):
     split_date = date_str.split('/')
-    year = int(split_date[2])
-    month = int(split_date[0])
-    day = int(split_date[1])
-    return datetime.date(year, month, day)
+    try:
+        year = int(split_date[2])
+        month = int(split_date[0])
+        day = int(split_date[1])
+        return_date =  datetime.date(year, month, day)
+    except (ValueError, IndexError):
+        input('''
+                \n****** DATE ERROR ******
+                \r The date format should include a valid date month/day/year
+                \rEX: 02/20/1984
+                \rPress enter to try again
+                \r**************************''')
+        return
+    else:
+        return return_date
    
 def clean_price(price_str):
-    slice_price = price_str[1:]
-    float_price = float(slice_price)
-    return int(float_price * 100)
+    try:
+        slice_price = price_str[1:]
+        float_price = float(slice_price)
+    except ValueError:
+        input('''
+                \n****** PRICE ERROR ******
+                \r The price should be a number without a currency symbol
+                \rEX: 23.8
+                \rPress enter to try again
+                \r**************************''')
+    else:
+        return int(float_price * 100)
     
     
     
@@ -70,7 +91,25 @@ def app():
     while app_running:
         choice = menu()
         if   choice == '1':
-            pass
+            product_name = input('Product name: ')
+            price_error = True
+            while price_error:
+                product_price = input('Product price: ')
+                product_price = clean_price(product_price)
+                if type(product_price) == int:
+                    price_error = False
+            product_quantity = input('Product quantity: ')
+            date_error = True
+            while date_error:
+                date_updated = input('date updated(Ex: 11/1/2018): ')
+                date_updated = clean_date(date_updated)
+                if type(date_updated) == datetime.date:
+                    date_error = False
+            new_product = Product(product_name = product_name, product_price= product_price, product_quantity=product_quantity, date_updated = date_updated)
+            session.add(new_product)
+            session.commit()
+            print('Product added to the inventory! ')
+            time.sleep(1.5)
         elif choice == '2':
             pass
         elif choice == '3':
@@ -87,5 +126,8 @@ def app():
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
     add_csv()
+    app()
+
     for product in session.query(Product):
         print(product)
+   
